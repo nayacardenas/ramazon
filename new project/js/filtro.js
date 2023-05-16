a1b3c76a011f599bc8e270d0396acafbd3ffc8c3
Marcelo Lopez <scsh3lo@gmail.com>
2023-05-15 23:20:10 -0400

    <pagina libro> filtra y carrito

--- new project/js/filtro.js
+++ new project/js/filtro.js
@@ -1,51 +1,31 @@
-$(document).ready(function() {
-    function filterItems() {
-        const selectedCategories = [];
-        const selectedLanguages = [];
-        const selectedAnio = [];
-        const selectedPrecio = [];
-        
-        $("input[type=checkbox][data-category]").each(function() {
-            if (this.checked) {
-                selectedCategories.push($(this).attr("data-category"));
-            }
+$(document).ready(function(){
+    function filtrar() {
+        // obtiene los productos seleccionados
+        var selectedCategories = [];
+        $('.category-checkbox:checked').each(function() {
+          selectedCategories.push($(this).val());
         });
-        
-        $("input[type=checkbox][data-language]").each(function() {
-            if (this.checked) {
-                selectedLanguages.push($(this).attr("data-language"));
-            }
-        });
+      
+        // Filtra los productos por categoría
+        if (selectedCategories.length === 0) {
+          $('.box').show();
+        } else {
+          
+          $('.box').hide();
+          selectedCategories.forEach(function(category) {
+            $('.box[data-category="' + category + '"]').show();
+          });
+        }
+      }
+      
+    
+      $('.category-checkbox').change(function() {
+        filtrar();
+      });
 
-        $("input[type=checkbox][data-anio]").each(function(){
-            if (this.checked){
-                selectedAnio.push($(this).attr("data-anio"))
-            }
 
-        });
 
-        $("input[type = checkbox][data-precio]").each(function(){
-            if (this.checked){
-                selectedPrecio.push($(this).attr("data-precio"))
-            }
-        });
-        
-        $(".producto").each(function() {
-            const itemCategory = $(this).attr("data-category");
-            const itemLanguage = $(this).attr("data-language");
-            const itemAnio = $(this).attr("data-anio");
-            const itemPrecio = $(this).attr("data-precio");
-            
-            if ((selectedCategories.length === 0 ||  selectedCategories.includes(itemCategory)) &&
-                (selectedLanguages.length === 0  ||  selectedLanguages.includes(itemLanguage)) &&
-                (selectedAnio.length === 0       ||  selectedAnio.includes(itemAnio)) &&
-                (selectedPrecio.length === 0     ||  selectedPrecio.includes(itemPrecio))){
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-    }
 
-    $("input[type=checkbox]").on("change", filterItems);
-});
+})
+
+  

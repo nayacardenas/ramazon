d7ddc5776146bb63bcfb90f3fa02ecf4a63b3ff9
Marcelo Lopez <scsh3lo@gmail.com>
2023-05-22 01:47:06 -0400

    mas cambios en el login

--- new project/js/login2.js
+++ new project/js/login2.js
@@ -1,17 +1,19 @@
-const login = document.querySelector('#login')
-login.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const name = document.querySelector('#usuario').value
-    const email = document.querySelector('#correo').value
-    const password = document.querySelector('#pass').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-
-    const validUser = Users.find(user => user.name === name && user.email === email && user.password === password)
-    if(!validUser){
-        return alert("datos incorrectos")
-    }
-    else{
+    const login = document.querySelector('#Login')
+    login.addEventListener('submit',(e)=>{
+        e.preventDefault();
+    
+        const name = document.querySelector('#usuario').value
+        const email = document.querySelector('#correo').value
+        const password = document.querySelector('#pass').value
+        const Users = JSON.parse(localStorage.getItem('users')) || []
+    
+    
+        const validUser = Users.find(user => user.name === name && user.email === email && user.password === password)
+        if(!validUser){
+            return alert("datos incorrectos")
+    
+    
+        }
         window.location.href="menu.html"
-    }
-   
-})
+       
+    });
--- new project/js/registrouser.js
+++ new project/js/registrouser.js
@@ -2,6 +2,7 @@ const registro = document.querySelector('#Registro')
 registro.addEventListener('submit',(e)=>{
     e.preventDefault();
    
+   
     const name = document.querySelector('#reg-name').value
     const apellido = document.querySelector('#reg-apellidos').value
     const email = document.querySelector('#reg-correo').value
@@ -12,11 +13,19 @@ registro.addEventListener('submit',(e)=>{
     if(isUserRegistered){
         return alert('el usuario ya esta registrado')
     }
+    if(!validarCorreo(email)){
+        return alert('por favor ingrese un correo valido')
+    }
     Users.push({name: name, apellido: apellido, email: email, password: password})
     localStorage.setItem('users',JSON.stringify(Users))
     alert('usuario registrado exitosamente')
     console.log('el usuario ha sido registrado');
-  
+    
+    function validarCorreo(correo){
+        var patronCorreo = /@+.+com/;
+        return patronCorreo.test(correo);
+    }
+    window.location.href="login.html"
     
     
 });

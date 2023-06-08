7b85c33171a8899728d51a2c3200abf2cdaa15f3
Marcelo Lopez <scsh3lo@gmail.com>
2023-06-08 17:06:29 -0400

    falta solucionar footer pag libro y conexiones

--- ramazonapp/urls.py
+++ ramazonapp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.viewIndex, name='index'),
+    path('sobreNosotros',views.viewAboutus, name='sobreNosotros'),
+    path('formulario',views.viewForm, name='formulario'),
+    path('tarjeta/',views.viewCard, name='tarjeta'),
+    path('/libros/',views.viewBooks, name='libros'),
 
 
 ]
--- ramazonapp/views.py
+++ ramazonapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def viewIndex(request):
     return render(request,'menu.html')
+
+def viewAboutus(request):
+    return render(request,'quienes somos .html')
+
+def viewForm(request):
+    return render(request,'formulario.html')
+
+def viewCard(request):
+    return render(request,'tarjeta.html')
+
+def viewBooks(request):
+    return render(request,'libros.html')
